0ae32c737a256206b23aa4338d88ba5765f3deb6
Mukund Dhananjay <mukund.d@phantom-fx.com>
2024-08-13 10:07:00 +0530

    Updated doc strings to Google/NumPy style

--- app_modules/usd/composers/ptx_base_composer.py
+++ app_modules/usd/composers/ptx_base_composer.py
@@ -32,27 +32,25 @@ class PhantomMatStruct:
 
 def convert_to_relative_path(abs_path: str, base_path: str) -> str:
     """
-    * Convert an absolute path to a relative path
-    * 
-    * @param abs_path:    type str:    The absolute path to convert
-    * @param base_path:   type str:    The base path to convert from
-    * 
-    * @return: type str
-    * 
+    Convert an absolute path to a relative path
+    
+    :param abs_path:    type str:    The absolute path to convert
+    :param base_path:   type str:    The base path to convert from
+    
+    :return: type str
     """
     return os.path.relpath(abs_path, base_path)
 
 
 def usd_stage(pfx_stage_path: str, stage_up_axis: UsdGeom.Tokens = UsdGeom.Tokens.y) -> Usd.Stage:
     """
-    * Take a path, and see if a USD stage exists here.
-    * If it exists, just open the stage and return it. If it doesn't, create a new usd stage
-    * at that location.
-    * @param pfx_stage_path:    type str:               The path of the USD stage on disk
-    * @param stage_up_axis:     type UsdGeom.Tokens:    The Up-Axis to use for this stage. By default, it's Y-Axis 
-    * 
-    * @return: type Usd.Stage
-    * 
+    Take a path, and see if a USD stage exists here.
+    If it exists, just open the stage and return it. If it doesn't, create a new usd stage
+    at that location.
+    :param pfx_stage_path:    type str:               The path of the USD stage on disk
+    :param stage_up_axis:     type UsdGeom.Tokens:    The Up-Axis to use for this stage. By default, it's Y-Axis 
+    
+    :return: type Usd.Stage 
     """
     usd_stage = None
     # Create the payload stage and save it
@@ -67,15 +65,14 @@ def usd_stage(pfx_stage_path: str, stage_up_axis: UsdGeom.Tokens = UsdGeom.Token
 
 def usd_root_prim(stage: Usd.Stage, root_prim_name: str, create_xform: bool = True) -> Usd.Prim:
     """
-    * See if the stage has a root prim, and if it exists, return it.
-    * If it doesn't, then create one, set it as the root and default prim, and return it.
-    *
-    * @param stage:             type Usd.Stage: The USD stage to add the root prim to
-    * @param root_prim_name:    type str:       The name of the Root Prim
-    * @param create_xform:      type bool:      Whether the Root Prim is an Xform or not. TODO: Support other types of prim
-    * 
-    * @return: type Usd.Prim
-    * 
+    See if the stage has a root prim, and if it exists, return it.
+    If it doesn't, then create one, set it as the root and default prim, and return it.
+    
+    :param stage:             type Usd.Stage: The USD stage to add the root prim to
+    :param root_prim_name:    type str:       The name of the Root Prim
+    :param create_xform:      type bool:      Whether the Root Prim is an Xform or not. TODO: Support other types of prim
+     
+    :return: type Usd.Prim 
     """
     if stage.GetPrimAtPath(f'/{root_prim_name}'):
         return stage.GetPrimAtPath(f'/{root_prim_name}')
@@ -92,12 +89,11 @@ def usd_root_prim(stage: Usd.Stage, root_prim_name: str, create_xform: bool = Tr
 
 def root_asset_info(root_prim: Usd.Prim, asset_path: str, asset_name: str):
     """
-    * Adds in the asset info on the given prim
-    * 
-    * @param root_prim:     type Usd.Prim:  The USD prim to add the asset info to 
-    * @param asset_path:    type str:       The asset path we're adding in
-    * @param asset_name:    type str:       The asset name  
-    *
+    Adds in the asset info on the given prim
+     
+    :param root_prim:     type Usd.Prim:  The USD prim to add the asset info to 
+    :param asset_path:    type str:       The asset path we're adding in
+    :param asset_name:    type str:       The asset name  
     """
     model_api = Usd.ModelAPI(root_prim)
     info = {
@@ -110,15 +106,14 @@ def root_asset_info(root_prim: Usd.Prim, asset_path: str, asset_name: str):
 
 def usd_reference(prim: Usd.Prim, asset_path: str, asset_prim_path: str) -> Sdf.Reference:
     """
-    * Search the prim for any reference with the asset_path and prim_path
-    * If it exists, return the reference, else, create it and return it.
-    * 
-    * @param prim:              type Usd.Prim:  The USD prim to add the reference to
-    * @param asset_path:        type str:       The asset path to refer in
-    * @param asset_prim_path:   type str:       The path of the prim in the asset we want to refer in
-    *
-    * @return: type Sdf.Reference
-    *
+    Search the prim for any reference with the asset_path and prim_path
+    If it exists, return the reference, else, create it and return it.
+     
+    :param prim:              type Usd.Prim:  The USD prim to add the reference to
+    :param asset_path:        type str:       The asset path to refer in
+    :param asset_prim_path:   type str:       The path of the prim in the asset we want to refer in
+    
+    :return: type Sdf.Reference
     """
     usd_ref = None
     if prim.HasAuthoredReferences():
@@ -139,15 +134,14 @@ def usd_reference(prim: Usd.Prim, asset_path: str, asset_prim_path: str) -> Sdf.
 
 def usd_mesh_payload(stage: Usd.Stage, prim_path: str, extern_payload_path: str, payload_prim_path: str) -> UsdGeom.Mesh:
     """
-    * Get the USD mesh prim with a payload, if it exists. If it doesn't exist, create one.
-    *
-    * @param stage:                     type Usd.Stage: The USD stage to add the scope to
-    * @param prim_path:                 type str:       The stage relative path of the primitive
-    * @param extern_payload_path:       type str:       The path to the payload on storage
-    * @param payload_prim_path:         type str:       The path of the payload in the alembic file
-    * 
-    * @return: type UsdGeom.Mesh
-    * 
+    Get the USD mesh prim with a payload, if it exists. If it doesn't exist, create one.
+    
+    :param stage:                     type Usd.Stage: The USD stage to add the scope to
+    :param prim_path:                 type str:       The stage relative path of the primitive
+    :param extern_payload_path:       type str:       The path to the payload on storage
+    :param payload_prim_path:         type str:       The path of the payload in the alembic file
+     
+    :return: type UsdGeom.Mesh 
     """
     if stage.GetPrimAtPath(f'{prim_path}'):
         return stage.GetPrimAtPath(f'{prim_path}')
@@ -165,15 +159,14 @@ def usd_mesh_payload(stage: Usd.Stage, prim_path: str, extern_payload_path: str,
 
 def usd_scope(stage: Usd.Stage, parent_prim: Usd.Prim = None, scope_name: str = "Scope") -> Usd.Prim:
     """
-    * Define a scope in the stage. If we have a valid parent prim, define the scope
-    * under the prim, else define it at the root.
-    *
-    * @param stage:         type Usd.Stage: The USD stage to add the scope to
-    * @param parent_prim:   type Usd.Prim:  The Parent Prim to add this scope under
-    * @param scope_name:    type Usd.Prim:  The name of the scope
-    * 
-    * @return: type Usd.Prim
-    * 
+    Define a scope in the stage. If we have a valid parent prim, define the scope
+    under the prim, else define it at the root.
+    
+    :param stage:         type Usd.Stage: The USD stage to add the scope to
+    :param parent_prim:   type Usd.Prim:  The Parent Prim to add this scope under
+    :param scope_name:    type Usd.Prim:  The name of the scope
+     
+    :return: type Usd.Prim 
     """
     geom_scope: Usd.Prim = stage.DefinePrim(f"{parent_prim.GetPath().pathString}/{scope_name}" if parent_prim else f"/{scope_name}", "Scope")
     return geom_scope
@@ -182,17 +175,16 @@ def usd_scope(stage: Usd.Stage, parent_prim: Usd.Prim = None, scope_name: str = 
 def usd_create_mtlx(stage: Usd.Stage, parent_prim: Usd.Prim = None, mtl_name: str = "MtlX", mtl_param_list: list = [],
                     mtlx_type:str = "/__class_mtl__/mtlxmaterial", shader_type:str = "ND_standard_surface_surfaceshader") -> UsdShade.Material:
     """
-    * Create a mtlx shader and override the parameters that need to be overridden.
-    *
-    * @param stage:         type Usd.Stage: The USD stage to add the mtlx to
-    * @param parent_prim:   type Usd.Prim:  The Parent Prim to add this mtlx under
-    * @param mtl_name:      type str:       The name of the material
-    * @param mtl_param_list: type list:     The list of parameters to override
-    * @param mtlx_type:     type str:       The type of mtlx shader. 
-    * @param shader_type:   type str:       The type of shader to create.
-    * 
-    * @return: type UsdShade.Material
-    * 
+    :Create a mtlx shader and override the parameters that need to be overridden.
+    
+    :param stage:          type Usd.Stage: The USD stage to add the mtlx to
+    :param parent_prim:    type Usd.Prim:  The Parent Prim to add this mtlx under
+    :param mtl_name:       type str:       The name of the material
+    :param mtl_param_list: type list:     The list of parameters to override
+    :param mtlx_type:      type str:       The type of mtlx shader. 
+    :param shader_type:    type str:       The type of shader to create.
+     
+    :return: type UsdShade.Material 
     """
     # Get an instance of our USDMaterialX Node
     p_fac = puf.PhantomUsdFactory()
@@ -256,19 +248,18 @@ def usd_create_mtlx(stage: Usd.Stage, parent_prim: Usd.Prim = None, mtl_name: st
 
 def usd_create_texture(stage: Usd.Stage, parent_prim: Usd.Prim, texture_path: str, param_name: str, uv_tile: ptusds.Float2 = ptusds.Float2(1.0, 1.0), mtl_name: str = "MtlX") -> tuple[UsdShade.Shader, UsdShade.Shader]:
     """
-    * Create a UsdUVTexture node, along with its associated uv coordinate node.
-    * We also handle creation for normal & tangent maps
-    *
-    * @param stage:         type Usd.Stage: The USD stage to create the texture under
-    * @param parent_prim:   type Usd.Prim:  The Parent Prim to add the texture under
-    * @param texture_path:  type str:       The path to the texture file
-    * @param param_name:    type str:       The name of the shader parameter we're adding to the material
-    * @param uv_tile:       type Float2:    The uv tiling value
-    * @param mtl_name:      type str:       The Material Name to which the texture belongs
-    * @param mtl_name:      type str:       The name of the material
-    *
-    * @return: type tuple(UsdShade.Shader, UsdShade.Shader)
-    *
+    Create a UsdUVTexture node, along with its associated uv coordinate node.
+    We also handle creation for normal & tangent maps
+    
+    :param stage:         type Usd.Stage: The USD stage to create the texture under
+    :param parent_prim:   type Usd.Prim:  The Parent Prim to add the texture under
+    :param texture_path:  type str:       The path to the texture file
+    :param param_name:    type str:       The name of the shader parameter we're adding to the material
+    :param uv_tile:       type Float2:    The uv tiling value
+    :param mtl_name:      type str:       The Material Name to which the texture belongs
+    :param mtl_name:      type str:       The name of the material
+    
+    :return: type tuple(UsdShade.Shader, UsdShade.Shader)
     """
     # define the Textures scope
     tex_scope_path = Sdf.Path(parent_prim.GetPath().AppendChild(f"{mtl_name}Textures"))
@@ -313,14 +304,13 @@ def usd_create_texture(stage: Usd.Stage, parent_prim: Usd.Prim, texture_path: st
 
 def usd_apply_material(prim: Usd.Prim, mtl_name: str, mtl: UsdShade.Material, mesh_list: List[UsdGeom.Mesh]):
     """
-    * Bind the given material to the list of meshes provided. We will use the collection method to bind the
-    * materials to the meshes
-    *
-    * @param prim:      type Usd.Prim:              The USD Prim which is the parent of the list of meshes
-    * @param mtl_name:  type str:                   Name of the Material we want to create the binding for
-    * @param mtl        type UsdShade.Material:     The Material Primitive to to create the binding for
-    * @param mesh_list: type List[UsdGeom.Mesh]:    The list of USD Meshes to bind the material to
-    *
+    Bind the given material to the list of meshes provided. We will use the collection method to bind the
+    materials to the meshes
+    
+    :param prim:      type Usd.Prim:              The USD Prim which is the parent of the list of meshes
+    :param mtl_name:  type str:                   Name of the Material we want to create the binding for
+    :param mtl        type UsdShade.Material:     The Material Primitive to to create the binding for
+    :param mesh_list: type List[UsdGeom.Mesh]:    The list of USD Meshes to bind the material to
     """
     mat_bind_api = UsdShade.MaterialBindingAPI.Apply(prim)
     collection_name = f"mat_bind_{mtl_name}"
@@ -336,17 +326,16 @@ def compose_pfx_usd(asset_info_path: str, asset_alembic_path: str,
                     usd_base_location: str, asset_type: str,
                     asset_name: str, asset_base_prim_path: str):
     """
-    * Main function for reading in the looks info to build the USD payload, asset and look files.
-    * This can be used as is, or can be used as a reference on how to use the methods in this module
-    * to build out a USD file from scratch.
-    *
-    * @param asset_info_path        : type str : Path to the asset json we'll be reading in
-    * @param asset_alembic_path     : type str : Path to the asset alembic path
-    * @param usd_base_location      : type str : Path to where the payload and asset USD files will be saved
-    * @param asset_type             : type str : The type of the asset
-    * @param asset_name             : type str : Name of the asset
-    * @param asset_base_prim_path   : type str : The base prim path of the asset, generally "/render_GRP"
-    *
+    Main function for reading in the looks info to build the USD payload, asset and look files.
+    This can be used as is, or can be used as a reference on how to use the methods in this module
+    to build out a USD file from scratch.
+    
+    :param asset_info_path        : type str : Path to the asset json we'll be reading in
+    :param asset_alembic_path     : type str : Path to the asset alembic path
+    :param usd_base_location      : type str : Path to where the payload and asset USD files will be saved
+    :param asset_type             : type str : The type of the asset
+    :param asset_name             : type str : Name of the asset
+    :param asset_base_prim_path   : type str : The base prim path of the asset, generally "/render_GRP"
     """
     # Create the main payload file if it doesn't exist.
     payload_usd_path = f"{usd_base_location}/Payload_{asset_type}_{asset_name}.usda"
